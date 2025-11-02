506b9a65f6e2afa880ad7dbe18b72147f2410106
FR <137614590+FilipRuman@users.noreply.github.com>
2025-11-02 15:17:53 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+//INFO: Default includes: use log::*;use anyhow::{Result,bail,Context};
+use anyhow::{Context, Result, bail};
+use log::*;
+
+fn main() {
+    colog::init();
+    info!("init colog");
+
+
+
+const FILE_PATH  = 
+    fs::read(path)
+}
---
+++ test_files/test.c
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int test(int value) {
+
+  // INFO: SOME comment
+  // Next comment
+  printf("real");
+
+  return 21;
+}
